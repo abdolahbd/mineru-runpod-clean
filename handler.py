855eaa29670f65ba7c2149172bbb989f544d9b1c
abdolah <abdolahbd@gmail.com>
2026-04-20 19:21:35 +0000

    Initial commit: Simple RunPod Worker

--- handler.py
+++ handler.py
@@ -1,10 +1,8 @@
 import runpod
 
-def handler(job):
-    return {
-        "success": True,
-        "message": "worker is alive",
-        "input": job.get("input", {})
-    }
+def my_handler(job):
+    job_input = job['input']
+    name = job_input.get('name', 'World')
+    return {"message": f"Hello {name} from RunPod! Worker is running successfully."}
 
-runpod.serverless.start({"handler": handler})
+runpod.serverless.start({"handler": my_handler})
